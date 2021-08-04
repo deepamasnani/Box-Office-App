b0991bc1800a3b70c36627311f5febedcdb27469
deepamasnani <asnani.deepam@gmail.com>
2021-08-04 14:50:04 +0530

    Learned that how to add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div >
-      HI
-    </div>
-  );
+  return <Switch>
+    <Route exact path="/">This is home page</Route>
+    <Route exact path="/starred">This is starred page</Route>
+    <Route>This is 404</Route>
+  </Switch>
 }
 
 export default App;
